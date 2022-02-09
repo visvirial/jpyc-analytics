a13841c67afe13cccd2235796724391f6396687e
Vis Virial <visvirial@gmail.com>
2022-02-09 19:36:06 +0900

    Change Nuxt.js build target to `server`.

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
 	// Target: https://go.nuxtjs.dev/config-target
-	target: 'static',
+	target: 'server',
 	
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
