1060accbdbadca58f64d24be7223413ebd899aac
Vis Virial <visvirial@gmail.com>
2022-02-10 19:11:58 +0900

    Enable on-memory cache in `/holders` API.

--- src/server.ts
+++ src/server.ts
@@ -72,48 +72,73 @@ export const main = async () => {
 		});
 	});
 	// /holders
+	const holdersCache = {
+		lastUpdate: 0,
+		holders: ([] as {
+			rank: number,
+			chain: string,
+			address: string,
+			value: Prisma.Decimal,
+		}[]),
+	};
 	app.get('/holders', async (req: express.Request, res: express.Response) => {
+		const computeHolders = async () => {
+			// Compute incoming values.
+			const ins = await prisma.transaction.groupBy({
+				by: ['chain', 'to'],
+				_sum: {
+					value: true,
+				},
+			});
+			// Compute outgoing values.
+			const outs = await prisma.transaction.groupBy({
+				by: ['chain', 'from'],
+				_sum: {
+					value: true,
+				},
+			});
+			// Concatinate `ins` and `outs`.
+			const holdersObj: { [address: string]: Prisma.Decimal } = {};
+			ins.forEach((input) => {
+				const key = `${input.chain}_${input.to}`;
+				holdersObj[key] = (holdersObj[key] || new Prisma.Decimal(0)).add(input._sum.value || new Prisma.Decimal(0));
+			});
+			outs.forEach((output) => {
+				const key = `${output.chain}_${output.from}`;
+				holdersObj[key] = (holdersObj[key] || new Prisma.Decimal(0)).sub(output._sum.value || new Prisma.Decimal(0));
+			});
+			let holders = [];
+			for(const chainAndAddress in holdersObj) {
+				const value = holdersObj[chainAndAddress];
+				const [chain, address] = chainAndAddress.split('_');
+				holders.push({
+					rank: -1,
+					chain,
+					address,
+					value,
+				});
+			}
+			// Discard zero balances.
+			holders = holders.filter((holder) => holder.value.isPositive() && !holder.value.isZero());
+			return holders;
+		};
 		const offset = Number.parseInt((req.query.offset as string) || '0');
 		const limit = Number.parseInt((req.query.limit as string) || '100');
 		const chainsRaw = ((req.query.chains as string) || 'all');
 		const chains = (chainsRaw === 'all' ? Object.keys(config.chains) : chainsRaw.split(','));
-		// Compute incoming values.
-		const ins = await prisma.transaction.groupBy({
-			by: ['chain', 'to'],
-			_sum: {
-				value: true,
-			},
-		});
-		// Compute outgoing values.
-		const outs = await prisma.transaction.groupBy({
-			by: ['chain', 'from'],
-			_sum: {
-				value: true,
-			},
-		});
-		// Concatinate `ins` and `outs`.
-		const holdersObj: { [address: string]: Prisma.Decimal } = {};
-		ins.forEach((input) => {
-			const key = `${input.chain}_${input.to}`;
-			holdersObj[key] = (holdersObj[key] || new Prisma.Decimal(0)).add(input._sum.value || new Prisma.Decimal(0));
-		});
-		outs.forEach((output) => {
-			const key = `${output.chain}_${output.from}`;
-			holdersObj[key] = (holdersObj[key] || new Prisma.Decimal(0)).sub(output._sum.value || new Prisma.Decimal(0));
-		});
-		let holders = [];
-		for(const chainAndAddress in holdersObj) {
-			const value = holdersObj[chainAndAddress];
-			const [chain, address] = chainAndAddress.split('_');
-			holders.push({
-				rank: -1,
-				chain,
-				address,
-				value,
-			});
-		}
-		// Discard zero balances.
-		holders = holders.filter((holder) => holder.value.isPositive() && !holder.value.isZero());
+		// Compute holders.
+		const getHolders = async () => {
+			const CACHE_DURATION = 10 * 1000;
+			if(holdersCache.lastUpdate > Date.now() - CACHE_DURATION) {
+				return holdersCache.holders;
+			} else {
+				const holders = await computeHolders();
+				holdersCache.lastUpdate = Date.now();
+				holdersCache.holders = holders;
+				return holders;
+			}
+		};
+		let holders = await getHolders();
 		// Extract records on desired chains.
 		holders = holders.filter((holder) => chains.indexOf(holder.chain) >= 0);
 		// Sort.
